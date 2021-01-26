ef2aa36dfe97efa3b41730edefef101aace10f27
Arnav G <pyeushgurha@pyeushs-imac.attlocal.net>
2021-01-25 18:46:22 -0800

    added_stuff

--- scripts.py
+++ scripts.py
@@ -1,17 +1,19 @@
 import argparse
 import os
-from github import Github
-from os import name, path
-import os
-import json
-import base64
-import bcrypt
-import getpass
-from cryptography.fernet import Fernet
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
+try:
+    from github import Github
+    from os import name, path
+    import os
+    import json
+    import base64
+    import bcrypt
+    import getpass
+    from cryptography.fernet import Fernet
+    from cryptography.hazmat.backends import default_backend
+    from cryptography.hazmat.primitives import hashes
+    from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+except Exception:
+    print("Ignore this comment if you do not wish to use github integration")
 def getkey(password,salt):
   kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),length=32,salt=salt,iterations=100000,backend= default_backend())
   key = base64.urlsafe_b64encode(kdf.derive(password))
